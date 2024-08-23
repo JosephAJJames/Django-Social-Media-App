c9d653aef96b8c244112addddb72df4368a02388
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-08-23 18:04:53 +0100

    Added signup button on login_page.html and created the page with the form

--- smapp/users/urls.py
+++ smapp/users/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login_user
+from .views import login_user, register_user
 
 app_name = "users"
 
 urlpatterns = [
-    path('login/', login_user, name="login")
+    path('login/', login_user, name="login"),
+    path('signup/', register_user, name="signup")
 ]
--- smapp/users/views.py
+++ smapp/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
@@ -18,3 +18,12 @@ def login_user(req):
             login(req, form.get_user())
             return redirect("mainpage:post_list")
        return HttpResponse("not worked")
+
+
+def register_user(req):
+    if req.method == "GET":
+        return render(req,
+        'users/sign_up.html',
+        context={
+            'signup_form': UserCreationForm
+        })
