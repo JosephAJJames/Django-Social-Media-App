1072c6b31648365525031288522a229258713310
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-09-18 16:10:21 +0100

    Basic styling and structre added to main.hmtl, changed the comment dict to pass objects rather than just the text

--- smapp/mainpage/views.py
+++ smapp/mainpage/views.py
@@ -20,12 +20,15 @@ def post_list(req):
 
     for y in comments:
         if y.post_id.id not in comment_dict:
-            comment_dict[y.post_id.id] = [y.text]
+            comment_dict[y.post_id.id] = [y]
         else:
-            comment_dict[y.post_id.id].append(y.text)
+            comment_dict[y.post_id.id].append(y)
 
 
     posts_with_forms = zip(posts, comment_forms)
+
+    print(comment_dict)
+
 
     return render(req, 'mainpage/main.html',
                   {'new_post': NewPostForm,
