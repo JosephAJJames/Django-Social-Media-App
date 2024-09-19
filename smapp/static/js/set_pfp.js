e47874c696386b658521c61393e006ef0f8590dc
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-09-19 16:26:11 +0100

    Styled edit profile

---
+++ smapp/static/js/set_pfp.js
@@ -0,0 +1,10 @@
+console.log("hello, world!");
+
+var pfp = document.getElementById("pfp");
+var file_input = document.getElementById("profilepic");
+
+
+pfp.addEventListener("click", () => {
+    file_input.click();
+})
+
